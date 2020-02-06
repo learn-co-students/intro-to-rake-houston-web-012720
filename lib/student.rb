9c8fef49ce7afbf4454797f2de2b6cb379ea8b02
placidowang <placidow@gmail.com>
2020-02-06 16:34:48 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+
   attr_accessor :name, :grade
   
   attr_reader :id
@@ -52,3 +52,4 @@ class Student
   end
 
 end
+
